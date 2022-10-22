282812585856b51564200bb1673f431c2c83efe4
Bekalu Endrias <bekalu2054@gmail.com>
2022-10-22 18:34:14 +0300

    fix

--- main.h
+++ main.h
@@ -15,9 +15,9 @@
  */
 typedef struct flags
 {
-    int plus;
-    int space;
-    int hash;
+	int plus;
+	int space;
+	int hash;
 } flags_t;
 
 /**
@@ -28,8 +28,8 @@ typedef struct flags
  */
 typedef struct printHandler
 {
-    char c;
-    int (*f)(va_list ap, flags_t *f);
+	char c;
+	int (*f)(va_list ap, flags_t *f);
 } ph;
 
 /* print_nums */
